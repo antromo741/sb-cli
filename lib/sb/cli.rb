d5c4c3828cd99739973f52017e80d3219a0d6424
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-03-15 20:19:52 +0000

    small changes

--- lib/sb/cli.rb
+++ lib/sb/cli.rb
@@ -10,13 +10,27 @@ class SmashCLI::CLI
   #menu to work 
   
   def call
-    
     puts "hello world:"
     puts "GETTING DATA FROM API..... PLEASE DANCE"
     puts "CREATING NEW OBJS"
+    display_info
   end
   
   
-  
+  def display_info
+    puts "please make a selection"
+    input = gets.strip.downcase
+    
+    if input == "player"
+      puts "============Charachters-list============"
+      puts "LIST OF PLAYERS/OBJECTS"
+    elsif input == "game"
+      puts "============GAME-LIST============"
+      puts "LIST OF GAMES/OBJECTS"
+      
+    else
+      puts "try again"
+  end
+end
   
 end
