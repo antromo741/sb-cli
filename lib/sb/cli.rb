978b3b605e288975a75c3242e5f16b8f4743d0b5
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-03-15 20:04:28 +0000

    adding text

--- lib/sb/cli.rb
+++ lib/sb/cli.rb
@@ -1,8 +1,22 @@
 #cli controller
 class SmashCLI::CLI
+  
+
+  #wanna take in input of a name 
+  #will give few options, 
+  #will give specific info
+  #conditional if value works
+  #exit command
+  #menu to work 
+  
   def call
     
     puts "hello world:"
-    #binding.pry
+    puts "GETTING DATA FROM API..... PLEASE DANCE"
+    puts "CREATING NEW OBJS"
   end
+  
+  
+  
+  
 end
