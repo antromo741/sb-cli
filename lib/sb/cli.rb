07009a90ccd50a7b3cc03f396a8a95bc9540cf19
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-03-15 00:36:05 +0000

    first commit

---
+++ lib/sb/cli.rb
@@ -0,0 +1,8 @@
+require "sb/cli/version"
+
+module Sb
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sb/cli/version.rb
@@ -0,0 +1,5 @@
+module Sb
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
