bb241de0f2ca78f3a4e9cb279e2aa49ea98994ad
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-03-17 18:43:42 +0000

    got more stuff to display

--- lib/sb/api.rb
+++ lib/sb/api.rb
@@ -4,9 +4,9 @@
  class PokeCLI::API 
    
     def self.get_pokemon(input)
-      @pokemon_hash = HTTParty.get("https://pokeapi.co/api/v2/pokemon-form/#{input}/")
-      #binding.pry
-   poke_obj = {
+      @pokemon_hash = HTTParty.get("https://pokeapi.co/api/v2/pokemon/1/")
+  poke_obj = {
+    
     name: @pokemon_hash["name"],
     id: @pokemon_hash["id"],
     base_experience: @pokemon_hash["base_experience"],
@@ -15,7 +15,7 @@
    
      
    } 
-  
+  binding.pry
    PokeCLI::Pokemon.new(poke_obj)
    end
    
